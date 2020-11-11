edeef59190777cacaed2ffc11eaaacde8039ead0
Ilari Puustinen <n9puil00@students.oamk.fi>
2020-11-11 19:48:08 +0200

    parallax alku

--- app/Views/kahvikauppa.php
+++ app/Views/kahvikauppa.php
@@ -1,1 +1,37 @@
-ETUSIVU
+<!-- ETUSIVU -->
+<h3><?= $title ?></h3>
+<form action="">
+<div id="bgimg-1" class="parallax">
+                    <div class="caption">
+                        <span class="border">Parallax</span>
+                    </div>
+                </div>
+                
+                <div class="text">
+                    <h3>Alpolla on existentiaalinen kriisi</h3>
+                </div>
+                
+                <div id="bgimg-2" class="parallax">
+                   
+                </div>
+                
+                <div class="text">
+                    <h3>Klonkku löysi sormuksen</h3>
+                </div>
+                
+                <div id="bgimg-3" class="parallax">
+                   
+                </div>
+                
+                <div class="text">
+                    <h3>Kolin huipulla</h3>
+                </div>
+                
+                <div id="bgimg-4" class="parallax">
+                    
+                </div>
+                
+                <div class="text">
+                    <h3>Testi</h3>
+                </div>
+</form>
