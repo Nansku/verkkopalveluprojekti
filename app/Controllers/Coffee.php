d345b4e891e5ef24d48cfa5ce46f53664cff4c87
Oskari Juntunen <n9juos00@students.oamk.fi>
2020-11-11 20:08:54 +0200

    Reitti yhteystietoihi

--- app/Controllers/Coffee.php
+++ app/Controllers/Coffee.php
@@ -4,11 +4,21 @@ class Coffee extends BaseController
 {
 	public function index()
 	{
+        $data['title'] = 'Kahvikauppa';
         echo view('template/header');
         echo view('kahvikauppa');
         echo view('template/footer');
 	}
 
+
+    public function contact_us()
+	{
+        $data['title'] = 'Contact_us';
+        echo view('template/header');
+        echo view('contact_us');
+        echo view('template/footer');
+	}
+	//-------
 	//--------------------------------------------------------------------
 
 }
