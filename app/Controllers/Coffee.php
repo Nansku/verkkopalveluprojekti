ec5d76f6d69e1072149dc6d41023cb1b81a04d79
Oskari Juntunen <n9juos00@students.oamk.fi>
2020-11-11 14:14:17 +0200

    Etusivu tullee osoitteella localhost:8080

--- app/Controllers/Coffee.php
+++ app/Controllers/Coffee.php
@@ -1,1 +1,16 @@
-INDEX
+<?php namespace App\Controllers;
+
+class Coffee extends BaseController
+{
+	public function index()
+	{
+        echo view('template/header');
+        echo view('kahvikauppa');
+        echo view('template/footer');
+	}
+
+	//--------------------------------------------------------------------
+
+}
+
+
