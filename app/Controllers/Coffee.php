5ffb5bcf0b2760061a712fe30fde4df4aeae2d36
Oskari Juntunen <n9juos00@students.oamk.fi>
2020-11-13 16:45:17 +0200

    navigaation linkit pitäs toimia

--- app/Controllers/Coffee.php
+++ app/Controllers/Coffee.php
@@ -20,7 +20,37 @@ class Coffee extends BaseController
 	}
 	//-------
 	//--------------------------------------------------------------------
-
+        public function story()
+	{
+        $data['title'] = 'Story';
+        echo view('template/header');
+        echo view('story');
+        echo view('template/footer');
+        }
+        
+        public function my_page()
+	{
+        $data['title'] = 'My Page';
+        echo view('template/header');
+        echo view('my_page');
+        echo view('template/footer');
+        }
+        
+        public function cart()
+	{
+        $data['title'] = 'Shopping Cart';
+        echo view('template/header');
+        echo view('cart');
+        echo view('template/footer');
+        }
+        
+        public function faq()
+	{
+        $data['title'] = 'FAQ';
+        echo view('template/header');
+        echo view('faq');
+        echo view('template/footer');
+	}
 }
 
 
