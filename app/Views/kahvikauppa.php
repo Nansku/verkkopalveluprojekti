8fb29340fd5c7bc135bee97c9880e7dd2ed2dbef
Ilari Puustinen <n9puil00@students.oamk.fi>
2020-11-13 16:45:47 +0200

    Etusivu lisäyksiä

--- app/Views/kahvikauppa.php
+++ app/Views/kahvikauppa.php
@@ -1,88 +1,10 @@
-<!--<style>
-body, html {
-    height: 100%; /* this is a must for parallax. */
-    font-family: Tahoma, Arial, sans-serif;
-    font-size: 100%;
-  }
-  
-  .parallax {
-    position: relative;
-    opacity: 0.8;
-    
-    /* Create the parallax scrolling effect */
-    background-attachment: fixed;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
-  
-  #kissa {
-    background-image: url(<?php echo base_url('img/9_kissa.jpg');?>);
-    height: 700px;
-  }
-  
-  #kettu {
-    background-image: url(<?php echo base_url('img/kettu.png');?>);
-    height: 700px;
-  }
-  
-  #Sauli {
-    background-image: url(<?php echo base_url('img/Sauli.jpg');?>);
-    height: 700px;
-  }
-  
-  #spirit {
-    background-image: url(<?php echo base_url('img/spirit.jpg');?>);
-    height: 700px;
-  }
-  
-  .caption {
-    position: absolute;
-    left: 0%;
-    top: 50%;
-    width: 100%;
-    text-align: center;  
-  }
-  
-  .caption span.border {
-    background-color: #111;
-    color: #fff;
-    padding: 18px;
-    font-size: 1.5em;
-    letter-spacing: 10px;
-    line-height: 2em;
-  }
-  
-  .text {
-      padding: 30px 100px 30px 100px;
-      position: relative;
-  }
-  
-  .text h3 {
-      text-align: center;
-  }
-  
-  /* Turn off parallax scrolling for all tablets and phones. 
-  Increase/decrease the pixels if needed */
-  @media screen and (max-width: 1366px) {
-      .parallax {
-          background-attachment: scroll;
-      }
-      
-      .text {
-          padding: 30px 10px 30px 10px;
-          position: relative;
-      }     
-  }
-</style>-->
-
 <div id="kissa" class="parallax">
                     <div class="caption">
                         <span class="border">Kahvia rölöm</span>
                     </div>
                 </div>
                 
-                <div class="text">
+                <div id="design" class="text">
                     <h3>Upea kissa</h3>
                 </div>
                 
@@ -90,7 +12,7 @@ body, html {
                    
                 </div>
                 
-                <div class="text">
+                <div id="design" class="text">
                     <h3>Kettu my friend</h3>
                 </div>
                 
@@ -98,7 +20,7 @@ body, html {
                    
                 </div>
                 
-                <div class="text">
+                <div id="design" class="text">
                     <h3>Sauli for president! Oh wait...</h3>
                 </div>
                 
@@ -106,17 +28,6 @@ body, html {
                     
                 </div>
                 
-                <div class="text">
-                    <h3>Me rakastamme kauneutta kohtuuden rajoissa, ja viisautta
-                        olematta silti velttoja. Rikkauttamme käytämme toimintaan
-                        oikeana hetkenä, emme siitä kerskaile, eikä kenellekään 
-                        ole häpeäksi, että tunnustaa köyhyytensä, mutta suuri 
-                        häpeä on, ellei hän tee mitä voi siitä vapautuakseen. 
-                        Samojen henkilöiden nähdään meillä hoitavan yksityisiä 
-                        ja yleisiä tehtäviä, ja nekin meistä, jotka rajoittuvat 
-                        oman ammattinsa harjoittamiseen, tuntevat varsin hyvin 
-                        julkiset asiat, sillä me yksin emme katso miestä, joka 
-                        ei osallistu valtiollisiin tehtäviin, ainoastaan 
-                        eristäytyväksi, vaan myös kelvottomaksi kansalaiseksi. 
-                        (Perikles n. 495-429 eKr.)</h3>
+                <div id="design" class="text">
+                    <h3>Spirit of the forest, what is your wisdom?</h3>
                 </div>
