c99ef734b10d2e8ac839330e287c22a980770394
Ville Rantanen <n9ravi00@students.oamk.fi>
2020-11-16 12:04:49 +0200

    Lisätty Contact Information.

--- app/Views/contact_us.php
+++ app/Views/contact_us.php
@@ -1,1 +1,197 @@
-YHTEYSTIEDOT
+<!-- Contact Information -->
+<div class="row text-center">
+   <div class="col">
+      <h2>Contact Information</h2>
+
+      <h4>Office and Storage</h4>
+
+      <p>Keskisentie 5, 84100 Ylivieska, Finland<br>
+      office@vienoscoffee.com</p>
+   </div>
+</div>
+
+<!-- Google Maps -->
+
+<!-- Eveliina -->
+<div class="row">
+   <div class="col mx-1">
+      <div class="card mb-3 mx-auto" style="max-width: 540px;">
+         <div class="row no-gutters">
+            <div class="col-4 text-center my-auto">
+               <!-- Icon is temporary -->
+               <i class="fas fa-user-ninja fa-6x"></i>
+               <!-- <img src=<?= base_url("img/eveliina.jpg") ?>
+               class="card-img" alt="Eveliina Purontaus"> -->
+            </div>
+            <div class="col-8">
+               <div class="card-body">
+                  <h5 class="card-title">Eveliina's title</h5>
+                  <p class="card-text">Eveliina Purontaus<br>
+                  eveliina@vienoscoffee.com<br>+358 40 123 4567</p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+
+<!-- Henri -->
+<div class="row">
+   <div class="col mx-1">
+      <div class="card mb-3 mx-auto" style="max-width: 540px;">
+         <div class="row no-gutters">
+            <div class="col-4 text-center my-auto">
+               <!-- Icon is temporary -->
+               <i class="fas fa-user-ninja fa-6x"></i>
+               <!-- <img src=<?= base_url("img/henri.jpg") ?>
+               class="card-img" alt="Henri Aukee"> -->
+            </div>
+            <div class="col-8">
+               <div class="card-body">
+                  <h5 class="card-title">Henri's title</h5>
+                  <p class="card-text">Henri Aukee<br>
+                  henri@vienoscoffee.com<br>+358 40 123 4567</p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+
+<!-- Ilari -->
+<div class="row">
+   <div class="col mx-1">
+      <div class="card mb-3 mx-auto" style="max-width: 540px;">
+         <div class="row no-gutters">
+            <div class="col-4 text-center my-auto">
+               <!-- Icon is temporary -->
+               <i class="fas fa-user-ninja fa-6x"></i>
+               <!-- <img src=<?= base_url("img/ilari.jpg") ?>
+               class="card-img" alt="Ilari Puustinen"> -->
+            </div>
+            <div class="col-8">
+               <div class="card-body">
+                  <h5 class="card-title">Ilari's title</h5>
+                  <p class="card-text">Ilari Puustinen<br>
+                  ilari@vienoscoffee.com<br>+358 40 123 4567</p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+
+<!-- Jami -->
+<div class="row">
+   <div class="col mx-1">
+      <div class="card mb-3 mx-auto" style="max-width: 540px;">
+         <div class="row no-gutters">
+            <div class="col-4 text-center my-auto">
+               <!-- Icon is temporary -->
+               <i class="fas fa-user-ninja fa-6x"></i>
+               <!-- <img src=<?= base_url("img/jami.jpg") ?>
+               class="card-img" alt="Jami Salmela"> -->
+            </div>
+            <div class="col-8">
+               <div class="card-body">
+                  <h5 class="card-title">Jami's title</h5>
+                  <p class="card-text">Jami Salmela<br>
+                  jami@vienoscoffee.com<br>+358 40 123 4567</p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+
+<!-- Jenna -->
+<div class="row">
+   <div class="col mx-1">
+      <div class="card mb-3 mx-auto" style="max-width: 540px;">
+         <div class="row no-gutters">
+            <div class="col-4 text-center my-auto">
+               <!-- Icon is temporary -->
+               <i class="fas fa-user-ninja fa-6x"></i>
+               <!-- <img src=<?= base_url("img/jenna.jpg") ?>
+               class="card-img" alt="Jenna Pennanen"> -->
+            </div>
+            <div class="col-8">
+               <div class="card-body">
+                  <h5 class="card-title">Jenna's title</h5>
+                  <p class="card-text">Jenna Pennanen<br>
+                  jenna@vienoscoffee.com<br>+358 40 123 4567</p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+
+<!-- Jukka -->
+<div class="row">
+   <div class="col mx-1">
+      <div class="card mb-3 mx-auto" style="max-width: 540px;">
+         <div class="row no-gutters">
+            <div class="col-4 text-center my-auto">
+               <!-- Icon is temporary -->
+               <i class="fas fa-user-ninja fa-6x"></i>
+               <!-- <img src=<?= base_url("img/jukka.jpg") ?>
+               class="card-img" alt="Jukka Liimatta"> -->
+            </div>
+            <div class="col-8">
+               <div class="card-body">
+                  <h5 class="card-title">Jukka's title</h5>
+                  <p class="card-text">Jukka Liimatta<br>
+                  jukka@vienoscoffee.com<br>+358 40 123 4567</p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+
+<!-- Oskari -->
+<div class="row">
+   <div class="col mx-1">
+      <div class="card mb-3 mx-auto" style="max-width: 540px;">
+         <div class="row no-gutters">
+            <div class="col-4 text-center my-auto">
+               <!-- Icon is temporary -->
+               <i class="fas fa-user-ninja fa-6x"></i>
+               <!-- <img src=<?= base_url("img/oskari.jpg") ?>
+               class="card-img" alt="Oskari Juntunen"> -->
+            </div>
+            <div class="col-8">
+               <div class="card-body">
+                  <h5 class="card-title">Oskari's title</h5>
+                  <p class="card-text">Oskari Juntunen<br>
+                  oskari@vienoscoffee.com<br>+358 40 123 4567</p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+
+<!-- Ville -->
+<div class="row">
+   <div class="col mx-1">
+      <div class="card mb-3 mx-auto" style="max-width: 540px;">
+         <div class="row no-gutters">
+            <div class="col-4 text-center my-auto">
+               <!-- Icon is temporary -->
+               <i class="fas fa-user-ninja fa-6x"></i>
+               <!-- <img src=<?= base_url("img/ville.jpg") ?>
+               class="card-img" alt="Ville Rantanen"> -->
+            </div>
+            <div class="col-8">
+               <div class="card-body">
+                  <h5 class="card-title">Ville's title</h5>
+                  <p class="card-text">Ville Rantanen<br>
+                  ville@vienoscoffee.com<br>+358 40 123 4567</p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
