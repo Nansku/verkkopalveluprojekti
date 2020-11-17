3eedb1d0e0c5763668508ca2d721e85526b8e689
n9peje00 <70717377+n9peje00@users.noreply.github.com>
2020-11-17 18:43:12 +0200

    cards olkaa hyvä

--- app/Views/products.php
+++ app/Views/products.php
@@ -1,16 +1,21 @@
 <div>
-   <?php foreach ($products as $product): ?>
-         <div class="row id1 d-flex justify-content-center">
-            <div class="col-4">
-               <h4><?= $product['productname'] ?></h4>
-                  <p><?=$product['description']?></p>
-                  <p><?=$product['price']?> €</p>
-            </div>
-            <div class="col-4">
-               <div class="float-right">
-                  <img src="<?= base_url('img/' . $product['picture'])?>"></img>
+   <?php foreach ($products as $product): ?> 
+      <div class=" d-flex justify-content-center">
+         <div class="card mt-4 mb-4" style="width: 1200px;">
+            <div class="row no-gutters id1">
+               <div class="col-8 card-body">
+                  <h4 class="card-title"><?= $product['productname'] ?></h4>
+                     <p class="card-text"><?=$product['description']?></p>
+                     <p class="card-text"><?=$product['price']?> €</p>
                </div>
-            </div>       
-        </div>
+            
+               <div class="col-4">
+                  <div class="float-right">
+                     <img class="card-img" src="<?= base_url('img/' . $product['picture'])?>"></img>
+                  </div>
+               </div> 
+            </div>      
+         </div>
+      </div>
    <?php endforeach; ?>
 </div>
