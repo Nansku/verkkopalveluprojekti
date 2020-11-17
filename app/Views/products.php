1242d5a6f6a947aa363d81c298b1d881bf42b145
n9peje00 <70717377+n9peje00@users.noreply.github.com>
2020-11-17 18:29:56 +0200

    keskitys

--- app/Views/products.php
+++ app/Views/products.php
@@ -1,12 +1,12 @@
 <div>
    <?php foreach ($products as $product): ?>
-         <div class="row id1">
-            <div class="col">
+         <div class="row id1 d-flex justify-content-center">
+            <div class="col-4">
                <h4><?= $product['productname'] ?></h4>
                   <p><?=$product['description']?></p>
                   <p><?=$product['price']?> €</p>
             </div>
-            <div class="col">
+            <div class="col-4">
                <div class="float-right">
                   <img src="<?= base_url('img/' . $product['picture'])?>"></img>
                </div>
