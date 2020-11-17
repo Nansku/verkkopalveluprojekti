a187706ab57b681c59fbe3a1ab86aeaf053c1d8f
Ilari Puustinen <n9puil00@students.oamk.fi>
2020-11-17 14:52:13 +0200

    Products.php & styles.css

--- app/Views/products.php
+++ app/Views/products.php
@@ -1,19 +1,27 @@
+<div class="row">
 <?php foreach ($products as $product): ?>
-   <h4><?= $product['productname'] ?></h4>
-<?php endforeach; ?>
-
-
-<?php /*
-foreach ($products as $product) {
-   if ($product['productID'] % 2 === 0) {
-   echo '<div class="row">
-      <div class="card mb-3" style="max-width: 540px;">
-         <div class="row no-gutters">
-            <div class="col-md-4">
-               <i class="fas fa-mug-hot fa-6x"></i>
-               <!-- <img src="" class="card-img" alt=""> -->
+         <div class="card container">
+            <div class="col-12">
+            <h4><?= $product['productname'] ?></h4>
+            <p><?=$product['description']?></p>
+            <p><?=$product['price']?> €</p>
+            <img src="<?= base_url('img/' . $product['picture'])?>"></img>
             </div>
-            <div class="col-md-8">
+        </div>
+<?php endforeach; ?>
+</div>
+
+ 
+   <!-- foreach ($products as $product) {
+   if ($product['productID'] % 2 === 0) {
+   echo '<div class="row tuote">
+      <div class="card mb-3">
+         <div class="row">
+            <div class="col-md-12">
+               <i class="fas fa-mug-hot fa-6x"></i>
+                <img src="" class="card-img" alt=""> 
+            </div>
+            <div class="col-md-12">
                <div class="card-body">
                   <h5 class="card-title">' . $product['productname'] . '</h5>
                   <p class="card-text">' . $product['description'] . '</p>
@@ -24,23 +32,22 @@ foreach ($products as $product) {
       </div>
    </div>';
    } else {
-      echo '<div class="row">
-      <div class="card mb-3" style="max-width: 540px;">
-         <div class="row no-gutters">
-            <div class="col-md-8">
+      echo '<div class="row tuote">
+      <div class="card mb-3">
+         <div class="row">
+            <div class="col-md-12">
                <div class="card-body">
                   <h5 class="card-title">' . $product['productname'] . '</h5>
                   <p class="card-text">' . $product['description'] . '</p>
                   <p class="card-text">' . $product['price'] . '</p>
                </div>
             </div>
-            <div class="col-md-4">
+            <div class="col-md-12">
                <i class="fas fa-mug-hot fa-6x"></i>
-               <!-- <img src="" class="card-img" alt=""> -->
+                <img src="" class="card-img" alt="">
             </div>
          </div>
       </div>
-   </div>';
+   </div>'; 
    }
-} */
-?>
+} -->
