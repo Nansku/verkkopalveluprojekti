5f06249831a4430ed4e4e9b81fa890cd3b21ea14
n9peje00 <70717377+n9peje00@users.noreply.github.com>
2020-11-21 15:03:47 +0200

    Rajausta kentille

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,17 +1,21 @@
-<h3>Login</h3>
-<form action="/login/check">
-    <div class="col-12">
-    <?= \Config\Services::validation()->listErrors(); ?>
+<div class=" d-flex justify-content-center">
+    <div class="col-5">
+    <h3>Login</h3>
+    <form action="/login/check">
+        <div class="col-12">
+        <?= \Config\Services::validation()->listErrors(); ?>
+        </div>
+        <div class="form-group">
+            <label>Email</label>
+            <input class="form-control" name="email" placeholder="Enter email address" maxlength="100">
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <input class="form-control" name="password" type="password" placeholder="Enter Password" maxlength="255">
+        </div>
+        <button class="btn btn-warning">Login</button>
+        <p>New customer?
+        <?= anchor('login/register','Register here') ?> </p>
+    </form>
     </div>
-    <div class="form-group">
-        <label>Email</label>
-        <input class="form-control" name="email" placeholder="Enter email address" maxlength="100">
-    </div>
-    <div class="form-group">
-        <label>Password</label>
-        <input class="form-control" name="password" type="password" placeholder="Enter Password" maxlength="255">
-    </div>
-    <button class="btn btn-warning">Login</button>
-    <p>New customer?
-    <?= anchor('login/register','Register here') ?> </p>
-</form>
+</div>
--- app/Views/register.php
+++ app/Views/register.php
@@ -1,39 +1,43 @@
-<h3>Register as user</h3>
-<form action="registration">
-    <div class="col-12">
-    <?= \Config\Services::validation()->listErrors(); ?>
-    </div>
-    <div class="form-group">
-        <label>Name</label>
-        <input class="form-control" name="name" placeholder="First and last name" maxlength="100">
-    </div>
-    <div class="form-group">
-        <label>Email</label>
-        <input class="form-control" name="email" placeholder="Enter email address" maxlength="255">
-    </div>
-    <div class="form-group">
-        <label>Address</label>
-        <input class="form-control" name="address" placeholder="Enter address" maxlength="100">
-    </div>
-    <div class="form-group">
-        <label>Postal code</label>
-        <input class="form-control" name="postalnumber" placeholder="Enter postal number" maxlength="5">
-    </div>
-    <div class="form-group">
-        <label>City</label>
-        <input class="form-control" name="city" placeholder="Enter city" maxlength="50">
-    </div>
-    <div class="form-group">
-        <label>Phone</label>
-        <input class="form-control" name="phonenumber" placeholder="Enter phonenumber" maxlength="10">
-    </div>
-    <div class="form-group">
-        <label>Password</label>
-        <input class="form-control" name="password" type="password" placeholder="Set password" maxlength="255">
-    </div>
-    <div class="form-group">
-        <label>Confirm password</label>
-        <input class="form-control" name="confirmpassword" type="password" placeholder="Enter password again" maxlength="255">
-    </div>
-    <button class="btn btn-warning">Lähetä</button>
-</form>
+<div class=" d-flex justify-content-center">
+    <div class="col-5">
+    <h3>Register as user</h3>
+    <form action="registration">
+        <div class="col-12">
+        <?= \Config\Services::validation()->listErrors(); ?>
+        </div>
+        <div class="form-group">
+            <label>Name</label>
+            <input class="form-control" name="name" placeholder="First and last name" maxlength="100">
+        </div>
+        <div class="form-group">
+            <label>Email</label>
+            <input class="form-control" name="email" placeholder="Enter email address" maxlength="255">
+        </div>
+        <div class="form-group">
+            <label>Address</label>
+            <input class="form-control" name="address" placeholder="Enter address" maxlength="100">
+        </div>
+        <div class="form-group">
+            <label>Postal code</label>
+            <input class="form-control" name="postalnumber" placeholder="Enter postal number" maxlength="5">
+        </div>
+        <div class="form-group">
+            <label>City</label>
+            <input class="form-control" name="city" placeholder="Enter city" maxlength="50">
+        </div>
+        <div class="form-group">
+            <label>Phone</label>
+            <input class="form-control" name="phonenumber" placeholder="Enter phonenumber" maxlength="10">
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <input class="form-control" name="password" type="password" placeholder="Set password" maxlength="255">
+        </div>
+        <div class="form-group">
+            <label>Confirm password</label>
+            <input class="form-control" name="confirmpassword" type="password" placeholder="Enter password again" maxlength="255">
+        </div>
+        <button class="btn btn-warning">Lähetä</button>
+    </form>
+</div>
+</div>
