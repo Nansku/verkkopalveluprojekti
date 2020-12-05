6a891b273c09c5a4be93c5d7e7e9b09e9dec33b7
Ilari Puustinen <n9puil00@students.oamk.fi>
2020-12-05 17:56:44 +0200

    Lisää Responsiivisuutta

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,5 +1,5 @@
 <div class=" d-flex justify-content-center mt-3 footer_margin">
-    <div class="col-5">
+    <div class="col-lg-6 col-md-6 col-sm-8">
     <h3>Login</h3>
     <form action="/login/check">
         <div class="col-12">
